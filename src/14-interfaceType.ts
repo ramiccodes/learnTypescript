186c4b3f78a7e5b9952d1a8d80108e94c87d3f48
ramiclaguitan <ramlag1819@gmail.com>
2024-08-22 21:47:47 -0700

    Interface challenge

--- src/14-interfaceType.ts
+++ src/14-interfaceType.ts
@@ -51,3 +51,52 @@ console.log(result);
 
 // Another way of setting up methods in interfaces
 // Example printSomething method above, utilizing arrow function syntax
+
+// ## Challenge
+
+// - Start by defining an interface Computer using the interface keyword. This will serve as a blueprint for objects that will be of this type.
+// - Inside the interface, define the properties that the object should have. In this case, we have id, brand, ram, and storage.
+// - Use the readonly keyword before the id property to indicate that it cannot be changed once it's set.
+// - Use the ? after the storage property to indicate that this property is optional and may not exist on all objects of this type.
+// - Also inside the interface, define any methods that the object should have. In this case, we have upgradeRam, which is a function that takes a number and returns a number.
+// - Now that we have our interface, we can create an object that adheres to this interface. This object should have all the properties defined in the interface (except for optional ones, which are... optional), and the methods should be implemented.
+// - Finally, we can use our object. We can call its upgradeRam method to increase its RAM.
+
+interface Computer {
+  readonly id: number;
+  brand: string;
+  ram: number;
+  storage?: number;
+  upgradeRam(ram: number): number;
+}
+
+const macbookPro: Computer = {
+  id: 1,
+  brand: "apple",
+  ram: 8,
+  upgradeRam(ram) {
+    return (this.ram = ram);
+  },
+};
+
+console.log(macbookPro.ram);
+
+macbookPro.upgradeRam(32);
+
+console.log(macbookPro.ram);
+
+const razerBlade: Computer = {
+  id: 2,
+  brand: "razer",
+  ram: 32,
+  storage: 256,
+  upgradeRam(ram) {
+    return (this.ram = ram);
+  },
+};
+
+console.log(razerBlade.ram);
+
+razerBlade.upgradeRam(64);
+
+console.log(razerBlade.ram);
